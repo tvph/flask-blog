66f2bd93cea6ec5ef3825f6671689a8bb84bc25e
Tran Viet Phuoc <phuoc.finn@gmail.com>
2019-10-04 01:00:22 +0700

    re-format flakes8

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,27 +6,27 @@ import os
 import datetime
 
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 
-app.config['SECRET_KEY']='1a94670cb42475d42189'
+app.config['SECRET_KEY'] = '1a94670cb42475d42189'
 
 # app.config.from_object(os.environ['APP_SETTINGS'])
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 # db=SQLAlchemy(app)
 
-posts=[
+posts = [
     {
         'author': 'tran viet phuoc',
         'title': 'blog post 1',
         'content': 'first blog content',
-        'date_posted': datetime.date(2019,8,20)
+        'date_posted': datetime.date(2019, 8, 20)
     },
     {
         'author': 'phuoc tran viet',
         'title': 'blog post 2',
         'content': 'second blog content',
-        'date_posted': datetime.date(2019,8,21)
+        'date_posted': datetime.date(2019, 8, 21)
     }
 ]
 
--- blog/manage.py
+++ blog/manage.py
@@ -1,13 +1,13 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app,db
+from app import app, db
 
-migrate=Migrate(app,db)
+migrate = Migrate(app, db)
 
-manager=Manager(app)
+manager = Manager(app)
 
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     manager.run()
