490e15a75a64d49c0e11466c80ff347f95c01d60
Tran Viet Phuoc <phuoc.finn@gmail.com>
2019-10-14 14:01:10 +0700

    re-format

--- blog/forms.py
+++ blog/forms.py
@@ -4,18 +4,20 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username=StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])
-    email=StringField('Email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
-    submit=SubmitField('Sign up')
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
 
     def __repr__(self):
-        return "'Username: '{}, 'Email: '{}".format(self.username,self.email)
+        return "'Username: '{}, 'Email: '{}".format(self.username, self.email)
+
 
 class LoginForm(FlaskForm):
-    email=StringField('Email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    submit=SubmitField('Log in')
-    remember=BooleanField('Remember me')
-    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
+    remember = BooleanField('Remember me')
