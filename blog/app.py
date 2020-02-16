6b867120f9053927e9323730b7e6a76b183a3c23
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-16 19:13:05 +0700

    added more layout

--- blog/app.py
+++ blog/app.py
@@ -18,13 +18,17 @@ posts = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
 # @app.route('register',method=['GET','POST'])
 # def register():
 
