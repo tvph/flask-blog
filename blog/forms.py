ffaa7ee4b0f62d6b0d7b179c7ec73df331639965
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-17 17:08:46 +0700

    update user account and make create new post template

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_wtf.file import FileField, FileAllowed
 from blog.models import User
+from flask_login import current_user
+from blog import crypt
 
 class RegistrationForm(FlaskForm):
 
@@ -23,6 +26,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose another one.')
+
+    
     # def __repr__(self):
     #     return "'Username: '{}, 'Email: '{}".format(self.username, self.email)
 
@@ -38,3 +43,32 @@ class LoginForm(FlaskForm):
 class SearchForm(FlaskForm):
     search = StringField(validators=[DataRequired()])
     submit = SubmitField('Search')
+
+class UpdateForm(FlaskForm):
+    email = StringField('Email:', validators=[DataRequired(), Email()])
+    password = PasswordField('Old Password:', validators=[DataRequired()])
+    new_password = PasswordField('New Password:', validators=[
+                                     DataRequired()])
+    confirm_password = PasswordField('Confirm New Password:', validators=[
+        DataRequired(), EqualTo('new_password')])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+
+    submit = SubmitField('Update')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose another one')
+    
+    def validate_password(self, password):
+        if password.data == current_user.password:
+            user = User.query.filter_by(username=self.email.data).first()
+            if crypt.generate_password_hash(password.data).decode('utf-8') != user.password:
+                raise ValidationError('Your old password is incorrect. Please try again')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,12 @@
 from flask import request, render_template, flash, redirect, url_for
-from blog.forms import RegistrationForm, LoginForm, SearchForm
+from blog.forms import RegistrationForm, LoginForm, SearchForm, UpdateForm, PostForm
 from blog.models import User, Post
 from blog import app, db, crypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
+
 
 # app.config.from_object(os.environ['APP_SETTINGS'])
 posts = []
@@ -12,14 +16,45 @@ posts = []
 @app.route('/home')
 def home():
     form = LoginForm()
-    return render_template('home.html', title='Home')
+    return render_template('home.html', posts=posts, title='Home')
 
 
-@app.route('/about')
+# save profile pictures into /static/pictures folder
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)  # make random hex string
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_name = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/pictures', picture_name)
+    # resize picture before save it
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumnail(output_size)
+    # save it
+    img.save(picture_path)
+    return picture_name
+
+
+@app.route('/about', methods=['GET', 'POST'])
 @login_required
 def about():
+    form = UpdateForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.picture = picture_file
+        
+        current_user.email = form.email.data
+        # hash new password and then save it
+        new_pwd_hashed = crypt.generate_password_hash(form.new_password.data)
+        current_user.password = new_pwd_hashed
+        db.session.commit()
+        flash(f'Your account have been update', 'success')
+        return redirect(url_for('about'))
+    elif request.method == 'GET':
+        form.email.data = current_user.email
+    # render to about page
     picture = url_for('static', filename='pictures/' + current_user.picture)
-    return render_template('about.html', title='About', picture=picture)
+    return render_template('about.html', title='About', picture=picture, form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -27,9 +62,12 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
+    # check validation when submit form in register page
     if form.validate_on_submit():
-        hashed_pwd = crypt.generate_password_hash(form.password.data).decode('utf-8')  # generate hashed password
-        user = User(username=form.username.data, email=form.email.data, password=hashed_pwd)
+        hashed_pwd = crypt.generate_password_hash(
+            form.password.data).decode('utf-8')  # generate hashed password
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_pwd)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account have been created! You are now able to log in', 'success')
@@ -37,7 +75,7 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -52,12 +90,24 @@ def login():
             flash(f'Login unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Log in', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
     return render_template('search.html')
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash(f'Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create.html', title='New post')
