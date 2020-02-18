89475ae4a408f4490a0bb44e082d1391e6877828
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-19 00:35:49 +0700

    updated

--- blog/__init__.py
+++ blog/__init__.py
@@ -9,20 +9,24 @@ from blog.config import Config
 
 # use flask_sqlalchemy extension
 db = SQLAlchemy()
+
 # use flask_bcrypt extension
 crypt = Bcrypt()
 
 # use flask-login extension to manage login
 login_manger = LoginManager()
-login_manger.login_view = 'users.login'
+login_manger.login_view = 'users.login'  # use blueprint
 login_manger.login_message_category = 'info'
+
 # use flask_mail exentsion
 mail = Mail()
 
-def creat_app(config_class=Config):
+
+def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # init all extensions with app
     db.init_app(app)
     crypt.init_app(app)
     login_manger.init_app(app)
@@ -37,7 +41,5 @@ def creat_app(config_class=Config):
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
-    
+
     return app
-
-
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from blog import creat_app  # this file use app.run() so import app here
+from blog import create_app  # this file use app.run() so import app here
 import os
 
-app = creat_app()
+app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
