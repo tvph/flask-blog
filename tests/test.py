d85667e85bcc9d82e21fe21d0147d1077e9ee950
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-20 00:41:20 +0700

    added tests folder

--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,30 @@ import pytest
 import unittest
 
 
-class TestExample:
-    def __init__(self):
-        pass
+# def add(a, b):
+#     return a+b
+# 
+# 
+# def test_add():
+#     assert add(2+3) == 6
+# 
+# 
+# def f():
+#     raise SystemExit(1)
+# 
+# 
+# def test_mytest():
+#     with pytest.raises(SystemExit):
+#         f()
 
+
+class TestClass:
+    def test_one(self):
+        x = 'this'
+        assert 'h' in x
+
+    def test_two(self):
+        x = 'hello'
+        assert hasattr(x, 'check')
 
 
