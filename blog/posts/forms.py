563ae64cfdbacd8d7dfcd259a9844cfe614f0293
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-19 09:21:01 +0700

    removed searchform class

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -7,8 +7,3 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
-
-
-class SearchForm(FlaskForm):
-    keyword = StringField('Keyword', validators=[DataRequired()])
-    submit = SubmitField('Submit')
