59cde9f785c910bc80df785557efa522dc77d382
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-19 13:40:11 +0700

    deploy app version 1.0

--- blog/posts/routes.py
+++ blog/posts/routes.py
@@ -3,7 +3,7 @@ from flask import (render_template, redirect, url_for, flash, request,
 from flask_login import current_user, login_required
 from blog import db
 from blog.models import Post
-from blog.posts.forms import PostForm, SearchForm
+from blog.posts.forms import PostForm
 
 
 posts = Blueprint('posts', __name__)
