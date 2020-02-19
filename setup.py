4c94ea5f11e00e112dfeecca89cf2fa4144bc80e
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-02-19 13:30:39 +0700

    deploy app version 1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Awesome Blog',
+    version='1.0',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+    )
